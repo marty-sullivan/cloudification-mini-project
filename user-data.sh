528734bfd320942e999d93bd1c4dd28286d63402
Marty Sullivan <mjs472@dhcp-humph-832.eduroam.cornell.edu>
2016-01-08 13:17:22 -0500

    Fix dir problems once and for all

--- user-data.sh
+++ user-data.sh
@@ -3,6 +3,6 @@
 yum update -y
 yum install -y git
 git clone https://github.com/marty-sullivan/cloudification-mini-project.git /home/ec2-user/cloudification-mini-project
-(cd /home/ec2-user/cloudification-mini-project/docker; docker build -t cloudification-rocks /home/ec2-user/cloudification-mini-project/docker)
+docker build -t cloudification-rocks /home/ec2-user/cloudification-mini-project/docker
 docker run -d --restart=always --name cloudification-rocks-live -p 80:80 cloudification-rocks
 
